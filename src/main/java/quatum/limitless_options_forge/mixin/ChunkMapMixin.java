647a3e46bf5f9890915e370fc7d7a99fb6c9fd2f
Quantum-mutnauQ <141065355+Quantum-mutnauQ@users.noreply.github.com>
2023-11-21 18:27:50 +0100

    Add Troll confing

---
+++ src/main/java/quatum/limitless_options_forge/mixin/ChunkMapMixin.java
@@ -0,0 +1,35 @@
+package quatum.limitless_options_forge.mixin;
+
+import net.minecraft.server.level.ChunkMap;
+import net.minecraft.util.Mth;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChunkMap.class)
+public class ChunkMapMixin{
+    @Mutable
+    @Shadow @Final
+    private static int MIN_VIEW_DISTANCE;
+    @Mutable
+    @Shadow @Final
+    public static int MAX_VIEW_DISTANCE ;
+    int anInt = 0;
+    int anInt_ = 0;
+
+    @Redirect(method = "setServerViewDistance", at = @At(value = "INVOKE", target = "net/minecraft/util/Mth.clamp(III)I"))
+    private int redirectClamp(int p_140168_, int min, int max) {
+        // Hier leitest du den Aufruf von Mth.clamp um und gibst einfach 'value' zurück
+        return Mth.clamp(p_140168_, 1, Byte.MAX_VALUE - 2);
+    }
+    @Inject(method = "<clinit>",at = @At("TAIL"))
+    private static void ViewdistancesFix(CallbackInfo ci){
+        MIN_VIEW_DISTANCE = 1;
+        MAX_VIEW_DISTANCE = Byte.MAX_VALUE -2;
+    }
+}
